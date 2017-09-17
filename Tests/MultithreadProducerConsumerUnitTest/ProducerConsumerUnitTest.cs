ee0802cab56d41fa69f694c50aabd54959b6a7be
Anderson Rancan <anderson.rancan@gmail.com>
2017-09-17 14:51:30 +0200

    General test setup

---
+++ Tests/MultithreadProducerConsumerUnitTest/ProducerConsumerUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using MultithreadProducerConsumer;
+
+namespace MultithreadProducerConsumerUnitTest
+{
+    [TestFixture]
+    public class ProducerConsumerUnitTest
+    {
+        private IProducerConsumer _testObject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testObject = new ProducerConsumer();
+        }
+
+        [Test]
+        public void StartingAndStoppingAConsumer_ShouldNotThrowExceptions()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                _testObject.StartConsumer();
+                _testObject.StopConsumer();
+            });
+        }
+    }
+}
